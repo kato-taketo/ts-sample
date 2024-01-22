516385427158517320d4a4f5a7365bf463d669ce
taketo <take10tokyohkit@gmail.com>
2024-01-22 17:22:10 +0900

    🔨 副作用追加, 未完成

--- react-sample/src/App.tsx
+++ react-sample/src/App.tsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [inputValue, setInputValue] = useState<string>('defaultValue');
+  const [response, setResponse] = useState<Response | null> (null);
+
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      fetch('api/data/list', {
+        body: JSON.stringify({ inputValue }),
+        signal: controller.signal
+      }).then((response) => {
+        setResponse(response);
+      });
+      return () => {
+        controller.abort();
+      };
+    },
+    [inputValue]
+  );
+
   return (
     <div className="App">
-      <input 
+      <input
         value={inputValue}
         onChange={(event) => {
           setInputValue(event.target.value);
         }}
-      />
+        />
       <div>{inputValue}</div>
     </div>
   );
 }
-
 export default App;
